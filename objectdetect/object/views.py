aa6d8ea0ceab9cd7c4feaa8b236fb13b1aecfe4f
chauhanjatin100 <chauhanjatin100@gmail.com>
2019-01-19 19:46:26 +0530

    protal done

--- objectdetect/object/views.py
+++ objectdetect/object/views.py
@@ -5,6 +5,9 @@ from django.shortcuts import render, redirect
 
 
 # -*- coding: utf-8 -*-
+import cv2
+from PIL import Image
+from .vgg import predict
 from .forms import ProfileForm
 from .models import QueryImage
 
@@ -15,16 +18,19 @@ def SaveImage(request):
     if request.method == "POST":
         # Get the posted form
 
-        print(request.POST['name'])
+        # print(request.POST['name'])
         MyProfileForm = ProfileForm(request.POST, request.FILES)
         # print(MyProfileForm.errors)
         if MyProfileForm.is_valid():
             # name = request.POST['name_image']
-
             image = QueryImage(query_image=request.FILES['picture'], name = request.POST['name'])
-
             # image.uploaded_at = MyProfileForm.cleaned_data["uploaded_at"]
+
             image.save()
+            read_image = Image.open('/home/jatin/codes/PanHack/objectdetect/media/' + str(image.query_image))
+            _, _, image_type = predict(read_image)
+            print(read_image)
+            print("problem type is -> ", image_type)
             saved = True
     else:
         MyProfileForm = ProfileForm()
